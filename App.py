6555b465d4b674d76d1799a439adb45d934d53ce
Kodomoppoi <jcmm.esft@gmail.com>
2024-06-11 01:44:10 -0300

    user db creation, starting html, started commiting info

--- App.py
+++ App.py
@@ -1,6 +1,4 @@
-from sqlalchemy import crate_engine, String, Interger, Column
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, String, Integer, Column
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request
 
@@ -32,25 +30,28 @@ class Fuse_Topics:
     
     def getdata(self):
         return self.x
-class AddComplexity:
+class AddExtra:
     def __init__(self, Fuse_Topics):
         self._Fuse_Topics = Fuse_Topics
 
-class AddType_Topic:
-    def __init__(self, Fuse_Topics):
-        self._Fuse_Topics = Fuse_Topics
+    def Type(self):
+        self.type = type
+
+    def addComplexity():
+        self.complexity = complexity
+
 
 
 #criação da db
-class Users(db.model):
+class Users(db.Model):
     __tablename__ = "users"
 
     #a db de cada usuario consistirá em  integer {{id}} único, string {{username}}, string {{topic_1}}, string {{topic_2}},integer {{complexity}}, string{{topic_type}}
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, unique = True, nullable = False)
-    topic_1 = db.Coluumn(db.String, nullable = False)
+    topic_1 = db.Column(db.String, nullable = False)
     topic_2 = db.Column(db.String, nullable = False)
-    complexity = db.Column(db.integer)
+    complexity = db.Column(db.Integer)
 
     def __init__(self, id, username, topic_1, topic_2, complexity):
         self.id = id
@@ -58,11 +59,43 @@ class Users(db.model):
         self.topic_1 = topic_1
         self.topic_2 = topic_2
         self.complexity = complexity
+
 #criação do back-end da pagina index, dar request e guardar na db criada
+
+@app.route('/', methods=['GET', 'POST'])
+
+def addUser():
+    if request.method == 'GET':      
+        return render_template('index.html')
+    elif request.method == 'POST':
+
+        username = request.form.get('username')
+        secret_key = request.form.get('senha')
+        topic_1 = request.form.get('topic_1')
+        topic_2 = request.form.get('topic_2')
+        complex_1 = request.form.get('complex_1')
+        complex_2 = request.form.get('complex_2')
+        type_1 = request.form.get('type_1')
+        type_2 = request.form.get('type_2')
+
+        if username == 'LUPO':
+            return "acertou o nome"
+        else:
+            return "NOOO!😡"
+
+        return render_template('index.html')
+        
+
+    return "nem entro no metodokkkkk otario"
+
+
+        
+
+
 
 
 
 if __name__ == "__main__":
     db.create_all()
-    app.run()
+    app.run(host = '0.0.0.0', debug = True)
 
